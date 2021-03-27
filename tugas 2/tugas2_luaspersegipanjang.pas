481c05cbf025e59ddaa811dbd59d22efb6f6e9bb
nadilanoerazozah@gmail.com <nadilanoerazizah@gmail.com>
2021-03-27 14:49:11 +0700

    tugas 2

---
+++ tugas 2/tugas2_luaspersegipanjang.pas
@@ -0,0 +1,14 @@
+{NAMA         : NADILA NOER AZIZAH  }
+{NPM          : 20.14.1.0056        }
+{NAMA PROGRAM : luas persegi panjang}
+
+program luas persegi panjang;
+uses crt;
+
+var
+lebar: integer;
+panjang: integer;
+luas: integer;
+
+begin
+writeln:
